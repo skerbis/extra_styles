394a2cd733009897723f141cd81da47c64bcfcc8
Thomas Skerbis <thomas.skerbis@mac.com>
2026-03-02 12:16:27 +0100

    Update boot.php

--- boot.php
+++ boot.php
@@ -22,28 +22,24 @@ if (rex::isBackend() && rex_be_controller::getCurrentPagePart(1) === 'extra_styl
     rex_view::addJsFile($addon->getAssetsUrl('extra_styles_backend.js'));
 }
 
-// Custom CSS überall einbinden (generierte Datei liegt unter /assets/addons/extra_styles/custom.css)
-$customCssUrl = rex_url::assets('addons/extra_styles/custom.css');
-
-if (rex::isBackend()) {
-    rex_view::addCssFile($customCssUrl);
-}
-
-// Frontend: Custom CSS einbinden
-if (!rex::isBackend()) {
-    rex_extension::register('OUTPUT_FILTER', function($ep) use ($customCssUrl) {
-        $content = $ep->getSubject();
-        $cssLink = '<link rel="stylesheet" href="' . $customCssUrl . '?' . time() . '">';
-        $content = str_replace('</head>', $cssLink . "\n</head>", $content);
-        return $content;
-    });
-}
-
-// CSS regenerieren wenn nötig
+// CSS regenerieren + einbinden nach PACKAGES_INCLUDED
 rex_extension::register('PACKAGES_INCLUDED', function() use ($addon) {
     if ($addon->getConfig('regenerate_css', false)) {
         CssGenerator::generate();
         $addon->setConfig('regenerate_css', false);
+    }
+
+    $customCssUrl = rex_url::assets('addons/extra_styles/custom.css');
+
+    if (rex::isBackend()) {
+        rex_view::addCssFile($customCssUrl);
+    } else {
+        rex_extension::register('OUTPUT_FILTER', function($ep) use ($customCssUrl) {
+            $content = $ep->getSubject();
+            $cssLink = '<link rel="stylesheet" href="' . $customCssUrl . '">';
+            $content = str_replace('</head>', $cssLink . "\n</head>", $content);
+            return $content;
+        });
     }
 });
 
